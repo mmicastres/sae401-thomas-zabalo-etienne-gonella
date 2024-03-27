9f76337cc1d7b0a5d850607144a88efbac4f0e85
SautDeBiquette <etienne.gonella@gmail.com>
2024-03-27 11:36:00 +0100

    test

--- app/Http/Controllers/PersonnagesController.php
+++ app/Http/Controllers/PersonnagesController.php
@@ -14,12 +14,14 @@ class PersonnagesController extends Controller
         if ($request->has('nom') && !empty($request->nom)) {
             $personnages = Personnages::where('nom', 'like', '%' . $request->nom . '%')->get();
             return response()->json($personnages, 200);
-        } elseif ($request->has('sous_race') && !empty($request->sous_race)) {
-            $personnages = Personnages::where('sous_race', 'like', '%' . $request->sous_race . '%')->get();
-            return response()->json("banane", 200);
         } else {
-            $personnages = Personnages::orderby('id', 'desc')->get();
-            return response()->json($personnages, 200);
+            if ($request->has('sous_race') && !empty($request->sous_race)) {
+                $personnages = Personnages::where('sous_race', 'like', '%' . $request->sous_race . '%')->get();
+                return response()->json("banane", 200);
+            } else {
+                $personnages = Personnages::orderby('id', 'desc')->get();
+                return response()->json($personnages, 200);
+            }
         }
     }
 
